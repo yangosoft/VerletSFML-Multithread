454cd1415955c69b1dbe121a879c3221fca8143f
Yangosoft <yangosoft@gmail.com>
2025-02-26 20:13:40 +0100

    Minor changes to through particles with mouse

--- src/engine/common/utils.hpp
+++ src/engine/common/utils.hpp
@@ -1,27 +1,24 @@
 #pragma once
 #include "index_vector.hpp"
+#include <SFML/Graphics.hpp>
 #include <sstream>
 
-
-template<typename U, typename T>
-U to(const T& v)
+template <typename U, typename T>
+U to(const T &v)
 {
     return static_cast<U>(v);
 }
 
-
-template<typename T>
+template <typename T>
 using CIVector = civ::Vector<T>;
 
-
-template<typename T>
+template <typename T>
 T sign(T v)
 {
     return v < 0.0f ? -1.0f : 1.0f;
 }
 
-
-template<typename T>
+template <typename T>
 static std::string toString(T value)
 {
     std::stringstream sx;
@@ -29,8 +26,7 @@ static std::string toString(T value)
     return sx.str();
 }
 
-
-template<typename T>
+template <typename T>
 sf::Vector2f toVector2f(sf::Vector2<T> v)
 {
     return {to<float>(v.x), to<float>(v.y)};
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 
-#include "engine/window_context_handler.hpp"
-#include "engine/common/color_utils.hpp"
 #include "engine/common/binary_io.hpp"
+#include "engine/common/color_utils.hpp"
+#include "engine/window_context_handler.hpp"
 
 #include "physics/physics.hpp"
-#include "thread_pool/thread_pool.hpp"
 #include "renderer/renderer.hpp"
-
+#include "thread_pool/thread_pool.hpp"
 
 int main()
 {
-    const uint32_t window_width  = 1920;
+    const uint32_t window_width = 1920;
     const uint32_t window_height = 1080;
     WindowContextHandler app("Verlet-MultiThread", sf::Vector2u(window_width, window_height), sf::Style::Default);
-    RenderContext& render_context = app.getRenderContext();
+    RenderContext &render_context = app.getRenderContext();
     // Initialize solver and renderer
 
     tp::ThreadPool thread_pool(10);
@@ -23,45 +22,46 @@ int main()
     Renderer renderer(solver, thread_pool);
 
     const float margin = 20.0f;
-    const auto  zoom   = static_cast<float>(window_height - margin) / static_cast<float>(world_size.y);
+    const auto zoom = static_cast<float>(window_height - margin) / static_cast<float>(world_size.y);
     render_context.setZoom(zoom);
     render_context.setFocus({world_size.x * 0.5f, world_size.y * 0.5f});
 
     bool emit = true;
-    app.getEventManager().addKeyPressedCallback(sf::Keyboard::Space, [&](sfev::CstEv) {
-        emit = !emit;
-    });
+    app.getEventManager().addKeyPressedCallback(sf::Keyboard::Space, [&](sfev::CstEv)
+                                                { emit = !emit; });
 
     constexpr uint32_t fps_cap = 60;
     int32_t target_fps = fps_cap;
-    app.getEventManager().addKeyPressedCallback(sf::Keyboard::S, [&](sfev::CstEv) {
+    app.getEventManager().addKeyPressedCallback(sf::Keyboard::S, [&](sfev::CstEv)
+                                                {
         target_fps = target_fps ? 0 : fps_cap;
-        app.setFramerateLimit(target_fps);
-    });
+        app.setFramerateLimit(target_fps); });
 
     sf::Image image;
     image.loadFromFile("res/test.png");
 
-    app.getEventManager().addKeyPressedCallback(sf::Keyboard::L, [&](sfev::CstEv) {
+    app.getEventManager().addKeyPressedCallback(sf::Keyboard::L, [&](sfev::CstEv)
+                                                {
         for (auto& obj : solver.objects) {
             IVec2 const pxl = static_cast<IVec2>(obj.position);
             sf::Color const color = image.getPixel(pxl.x, pxl.y);
             obj.color = color;
-        }
-    });
+        } });
 
-    app.getEventManager().addKeyPressedCallback(sf::Keyboard::W, [&](sfev::CstEv) {
+    app.getEventManager().addKeyPressedCallback(sf::Keyboard::W, [&](sfev::CstEv)
+                                                {
         BinaryWriter writer{"colors.bin"};
         for (auto& obj : solver.objects) {
             writer.write(obj.color);
-        }
-    });
+        } });
 
     std::vector<sf::Color> colors;
 
     BinaryReader color_reader{"colors.bin"};
-    if (color_reader.isValid()) {
-        for (uint32_t i{20000}; i--;) {
+    if (color_reader.isValid())
+    {
+        for (uint32_t i{20000}; i--;)
+        {
             colors.push_back(color_reader.read<sf::Color>());
         }
     }
@@ -69,16 +69,47 @@ int main()
 
     // Main loop
     const float dt = 1.0f / static_cast<float>(fps_cap);
-    while (app.run()) {
-        if (solver.objects.size() < 20000 && emit) {
-            for (uint32_t i{20}; i--;) {
-                const auto id = solver.createObject({2.0f, 10.0f + 1.1f * i});
-                solver.objects[id].last_position.x -= 0.2f;
-                if (colors.empty()) {
+    sf::Vector2f position;
+    auto last_emit = std::chrono::high_resolution_clock::now();
+    while (app.run())
+    {
+
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+        {
+            auto now = std::chrono::high_resolution_clock::now();
+            if (now - last_emit > std::chrono::milliseconds(300))
+            {
+                last_emit = now;
+                emit = true;
+                position = app.getWorldMousePosition();
+            }
+        }
+        else
+        {
+            emit = false;
+        }
+
+        if (solver.objects.size() < 20000 && emit)
+        {
+            emit = false;
+            for (uint32_t i{10}; i--;)
+            {
+
+                position.x = position.x + 1.0f * i;
+                const auto id = solver.createObject(position);
+                // solver.objects[id].last_position.x -= 0.2f;
+                solver.objects[id].last_position.y -= 0.1f;
+                solver.objects[id].acceleration.x = 0.0f;
+                solver.objects[id].acceleration.y = 0.0f;
+                solver.objects[id].color = ColorUtils::getRainbow(id * 0.1f);
+                /*if (colors.empty())
+                {
                     solver.objects[id].color = ColorUtils::getRainbow(id * 0.0001f);
-                } else {
-                    solver.objects[id].color = colors[id];
                 }
+                else
+                {
+                    solver.objects[id].color = colors[id];
+                }*/
             }
         }
         solver.update(dt);
